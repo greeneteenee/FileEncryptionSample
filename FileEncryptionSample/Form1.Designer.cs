e9b2c613cb4ad1580f66b00c003937fa6016c6e3
Tina Greene <46331543+greeneteenee@users.noreply.github.com>
2019-02-18 15:26:16 -0800

    Created a simple encrypt decrypt program which takes a txt file and encrypts it or decrypts it

--- FileEncryptionSample/Form1.Designer.cs
+++ FileEncryptionSample/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.rbEncrypt = new System.Windows.Forms.RadioButton();
+            this.rbDecrypt = new System.Windows.Forms.RadioButton();
+            this.txtShowFile = new System.Windows.Forms.TextBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtSaveFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(62, 37);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(233, 108);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open file";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(62, 225);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(233, 108);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save file";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // rbEncrypt
+            // 
+            this.rbEncrypt.AutoSize = true;
+            this.rbEncrypt.Location = new System.Drawing.Point(136, 168);
+            this.rbEncrypt.Name = "rbEncrypt";
+            this.rbEncrypt.Size = new System.Drawing.Size(61, 17);
+            this.rbEncrypt.TabIndex = 2;
+            this.rbEncrypt.TabStop = true;
+            this.rbEncrypt.Text = "Encrypt";
+            this.rbEncrypt.UseVisualStyleBackColor = true;
+            this.rbEncrypt.CheckedChanged += new System.EventHandler(this.rbEncrypt_CheckedChanged);
+            // 
+            // rbDecrypt
+            // 
+            this.rbDecrypt.AutoSize = true;
+            this.rbDecrypt.Location = new System.Drawing.Point(136, 191);
+            this.rbDecrypt.Name = "rbDecrypt";
+            this.rbDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.rbDecrypt.TabIndex = 3;
+            this.rbDecrypt.TabStop = true;
+            this.rbDecrypt.Text = "Decrypt";
+            this.rbDecrypt.UseVisualStyleBackColor = true;
+            this.rbDecrypt.CheckedChanged += new System.EventHandler(this.rbDecrypt_CheckedChanged);
+            // 
+            // txtShowFile
+            // 
+            this.txtShowFile.Location = new System.Drawing.Point(329, 37);
+            this.txtShowFile.Multiline = true;
+            this.txtShowFile.Name = "txtShowFile";
+            this.txtShowFile.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtShowFile.Size = new System.Drawing.Size(314, 296);
+            this.txtShowFile.TabIndex = 4;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(329, 11);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(313, 20);
+            this.txtFilePath.TabIndex = 5;
+            // 
+            // txtSaveFilePath
+            // 
+            this.txtSaveFilePath.Location = new System.Drawing.Point(329, 339);
+            this.txtSaveFilePath.Name = "txtSaveFilePath";
+            this.txtSaveFilePath.Size = new System.Drawing.Size(313, 20);
+            this.txtSaveFilePath.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 374);
+            this.Controls.Add(this.txtSaveFilePath);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.txtShowFile);
+            this.Controls.Add(this.rbDecrypt);
+            this.Controls.Add(this.rbEncrypt);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RadioButton rbEncrypt;
+        private System.Windows.Forms.RadioButton rbDecrypt;
+        private System.Windows.Forms.TextBox txtShowFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtSaveFilePath;
     }
 }
 
--- FileEncryptionSample/Form1.cs
+++ FileEncryptionSample/Form1.cs
@@ -3,18 +3,138 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace FileEncryptionSample
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            
         }
+
+        private string generateKey()
+        {
+            DESCryptoServiceProvider dESCrypto = (DESCryptoServiceProvider)DESCryptoServiceProvider.Create();
+            return ASCIIEncoding.ASCII.GetString(dESCrypto.Key);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = false;//user can only open one file
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                //get path of file
+                string filePath = open.FileName;
+                txtFilePath.Text = filePath;
+
+                //read contents of file into a stream
+                var fileStream = open.OpenFile();
+
+                using(StreamReader reader = new StreamReader(filePath))
+                {
+                    txtShowFile.Text = reader.ReadToEnd();
+                }
+
+            }
+
+            btnSave.Enabled = true;
+
+        }
+
+        private void rbEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbEncrypt.Checked)
+            {
+                rbDecrypt.Checked = false;
+            }
+        }
+
+        private void rbDecrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDecrypt.Checked)
+            {
+                rbEncrypt.Checked = false;
+            }
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbEncrypt.Checked = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtFilePath.Text))
+            {
+                MessageBox.Show("Must open file first");
+                return;
+            }
+
+            string hash = "f0xle@rn";
+            
+            if(rbEncrypt.Checked == true)
+            {
+                encrypt(txtShowFile.Text, hash);
+            }
+            else
+            {
+                decrypt(txtShowFile.Text, hash);
+            }
+
+            SaveFileDialog save = new SaveFileDialog();
+            if(save.ShowDialog() == DialogResult.OK)
+            {
+                string saveFileName = save.FileName;
+                File.WriteAllText(saveFileName, txtShowFile.Text);
+                txtSaveFilePath.Text = save.FileName;
+            }
+
+            btnSave.Enabled = false;
+       
+        }
+
+        private void encrypt(string input, string StrHash)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(input);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(StrHash));
+                using(TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    txtShowFile.Text = "";
+                    txtShowFile.Text = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        private void decrypt(string input, string StrHash)
+        {
+            byte[] data = Convert.FromBase64String(input);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(StrHash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    txtShowFile.Text = "";
+                    txtShowFile.Text = UTF8Encoding.UTF8.GetString(results);
+                }
+            }
+        }
+
     }
 }
